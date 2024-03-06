2627145dc5f6c1eb1ab95590275a4fdadadf3f8b
fritzlb <106099434+fritzlb@users.noreply.github.com>
2024-03-06 12:30:44 +0100

    Update shortut.bat

--- shortut.bat
+++ shortut.bat
@@ -2,3 +2,5 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 python jit_enabler_better.py bundle_ID
+
+exit
