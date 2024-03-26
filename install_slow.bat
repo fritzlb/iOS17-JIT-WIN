0b3211898437bcb87b7a952279fed2d0f65f048e
fritzlb <106099434+fritzlb@users.noreply.github.com>
2024-03-26 23:26:16 +0100

    Update install_slow.bat

--- install_slow.bat
+++ install_slow.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/mstorsjo/llvm-mingw/releases/download/20231128/llvm-mingw-20231128-msvcrt-x86_64.zip', 'archive.zip') }"
 powershell -command "Expand-Archive archive.zip ."
 del archive.zip
-pip install pymobiledevice3
+pip install pymobiledevice3==2.46.2
 
 
 @echo off
